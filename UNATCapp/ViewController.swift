546305eb6f2d080e39a1e033bd67b18bafb25e28
Andrei Antal <andrei.antal@qualitance.ro>
2015-02-06 15:14:13 +0200

    Initial Commit

---
+++ UNATCapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UNATCapp
+//
+//  Created by Andrei Antal on 06/02/15.
+//  Copyright (c) 2015 Andrei Antal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
